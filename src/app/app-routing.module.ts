3c74f8e7a27c41be107d75d1f962b04aecc788cf
28javier <javier19952009@gmail.com>
2020-10-18 11:29:47 -0500

    Modulos y Router separados de los principales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { CategoriaComponent } from './pages/categoria/categoria.component';
-import { UsuarioComponent } from './pages/usuario/usuario.component';
-import { ProductoComponent } from './pages/producto/producto.component';
-import { PacienteComponent } from './pages/paciente/paciente.component';
-import { LoginComponent } from './auth/login/login.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
+// Importacion del modulo de rutas hijas
+import { PagesRoutingModule } from './pages/pages.routing';
+import {  AuthRoutingModule } from './auth/auth.routing';
+
+// Importacion de modulos componentes
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 
-const routes:Routes = [
-
-  {path: '',
-   component: PagesComponent,
-   children: [
-    {path: 'dashboard', component: DashboardComponent},
-    {path: 'categoria', component: CategoriaComponent},
-    {path: 'usuario', component: UsuarioComponent},
-    {path: 'producto', component: ProductoComponent},
-    {path: 'paciente', component: PacienteComponent},
-    {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-   ]
-  },
-  {path: 'login', component: LoginComponent},
+const routes: Routes = [
+// path: '/dashboard', PagesRoutingModule
+// path: '/auth', AuthRoutingModule
   {path: '**', component: NopagefoundComponent}
 ];
 
@@ -32,8 +19,10 @@ const routes:Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
-  exports:[RouterModule]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,38 +4,30 @@ import { NgModule } from '@angular/core';
 // ruta principal ver
 import { AppRoutingModule } from './app-routing.module';
 
+// Importacion del Modulo que compone las paginas principales
+import { PagesModule } from './pages/pages.module';
+
+// Importcacion del Modulo de auth para el modulo principal que es este
+import { AuthModule } from './auth/auth.module';
+
 // componentes
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { UsuarioComponent } from './pages/usuario/usuario.component';
-import { ProductoComponent } from './pages/producto/producto.component';
-import { CategoriaComponent } from './pages/categoria/categoria.component';
-import { PacienteComponent } from './pages/paciente/paciente.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { PagesComponent } from './pages/pages.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    UsuarioComponent,
-    ProductoComponent,
-    CategoriaComponent,
-    PacienteComponent,
-    BreadcrumbsComponent,
-    SidebarComponent,
-    HeaderComponent,
-    PagesComponent
+    NopagefoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
